4beb5875a2daaa8528f7ef67c95c0a0793882f17
mppolak <emilgojny@gmail.com>
2016-06-28 10:49:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,19 @@ if [ $(which gcc) ]; then
 	gcc -lm -o 3dec2frac 3dec2frac.c
 	gcc -lm -o 3frac2num 3frac2num.c
 	chmod +x kpr2ksc
+	echo
 	echo "If you didnt see any errors, the installation finished correctly"
+	echo
 elif [ $(which icc) ]; then
 	icc -o 3dec2frac 3dec2frac.c
 	icc -o 3frac2num 3frac2num.c
 	chmod +x kpr2ksc
+	echo
 	echo "If you didnt see any errors, the installation finished correctly"
+	echo
 
 else
+	echo
 	echo "I cant find a C compiler, compile the two *.c files yourself"
+	echo
 fi
